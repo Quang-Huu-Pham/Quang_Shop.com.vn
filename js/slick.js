b926c87b34e7dbab1776073ab0e35aa654658171
unknown <ilove060902@gmail.com>
2022-01-18 19:39:56 +0700

    load

--- js/slick.js
+++ js/slick.js
@@ -1,21 +1,23 @@
-$('.slider__products').slick({
-    infinite: false,
-    slidesToShow: 6,
-    slidesToScroll: 2,
-    prevArrow: `<button type='button' class='slick-prev arrow'><i class="bx bxs-chevron-left" ></i></button>`,
-    nextArrow: `<button type='button' class='slick-next arrow'><i class="bx bxs-chevron-right"></i></button>`,
-    responsive: [{
-            breakpoint: 1025,
-            settings: {
-                slidesToShow: 4,
+$(document).ready(function() {
+    $('.slider__products').slick({
+        infinite: false,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        prevArrow: `<button type='button' class='slick-prev arrow'><i class="bx bxs-chevron-left" ></i></button>`,
+        nextArrow: `<button type='button' class='slick-next arrow'><i class="bx bxs-chevron-right"></i></button>`,
+        responsive: [{
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 4,
+                },
             },
-        },
-        {
-            breakpoint: 737,
-            settings: {
-                slidesToShow: 2,
-                arrows: false,
+            {
+                breakpoint: 737,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                },
             },
-        },
-    ],
+        ],
+    });
 });
